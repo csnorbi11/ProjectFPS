20ba994f8d5c097b1651e351137d0f1777c633d0
csnorbi11 <csaszar.norbi@gmail.com>
2025-06-06 19:45:10 +0200

    fragment shader updated

--- assets/shaders/fragment.glsl
+++ assets/shaders/fragment.glsl
@@ -45,6 +45,7 @@ uniform vec3 viewPos;
 vec3 CalcDirLight(DirectionalLight light,vec3 normal,vec3 viewDir);
 vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
 
+
 void main()
 {
     vec3 norm=normalize(Normal);
@@ -65,8 +66,8 @@ vec3 CalcDirLight(DirectionalLight light,vec3 normal,vec3 viewDir){
     vec3 lightDir=normalize(-light.direction);
     float diff=max(dot(normal,lightDir),0);
 
-    vec3 reflectDir=reflect(-lightDir,normal);
-    float spec=pow(max(dot(viewDir,reflectDir),0.f),material.shininess);
+    vec3 halfway= normalize(lightDir + viewDir);
+    float spec=pow(max(dot(normal,halfway),0.f),material.shininess);
 
     vec3 ambient=light.ambient*Ambient;
     vec3 diffuse=light.diffuse*diff*Diffuse;
@@ -85,8 +86,8 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
     vec3 lightDir = normalize(light.position - fragPos);
     float diff = max(dot(normal, lightDir), 0.0);
 
-    vec3 reflectDir = reflect(-lightDir, normal);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 halfway= normalize(lightDir + viewDir);
+    float spec=pow(max(dot(normal,halfway),0.f),material.shininess);
 
     float distance    = length(light.position - fragPos);
     float attenuation = 1.0 / (light.constant + light.linear * distance +
