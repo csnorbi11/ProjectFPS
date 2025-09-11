746a68659979f6ceb9bc17378c76f202d794fff8
Császár Norbert <csaszar.norbi@gmail.com>
2025-09-11 23:50:58 +0200

    packed glad use

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,17 +1,17 @@
-# fordítók
+# fordï¿½tï¿½k
 if(NOT WIN32)
 	set(CMAKE_C_COMPILER clang CACHE STRING "" FORCE)
 	set(CMAKE_CXX_COMPILER clang++ CACHE STRING "" FORCE)
 endif()
 
 
-# globális figyelmeztetések
+# globï¿½lis figyelmeztetï¿½sek
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wdeprecated -pedantic")
 
-# build típus specifikus
+# build tï¿½pus specifikus
 set(CMAKE_CXX_FLAGS_DEBUG "-g -O0")
 set(CMAKE_CXX_FLAGS_RELEASE "-O3")
 
-# kimeneti mappák
+# kimeneti mappï¿½k
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_BINARY_DIR}/Debug)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_BINARY_DIR}/Release)
